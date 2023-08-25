56c30884f4421cf4d7d9c1c1898814f51a8dcfc2
Nipun Upekshana <nipun.upekshana@itelasoft.com>
2023-08-25 16:45:38 +0530

    added googl tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import config from "@config/config.json";
 import { Head, Html, Main, NextScript } from "next/document";
+import Script from 'next/script'
 
 const Document = () => {
   // destructuring items from config object
@@ -7,6 +8,16 @@ const Document = () => {
   return (
     <Html lang="en">
       <Head>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {
+            `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NRQCGD32');
+            `
+          }
+        </Script>
         {/* favicon */}
         <link
           rel="shortcut icon"
@@ -29,6 +40,11 @@ const Document = () => {
         />
       </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NRQCGD32" height="0" width="0" style="display: none; visibility: hidden;" />`,
+          }}
+        />
         <Main />
         {/* <TwSizeIndicator /> */}
         <NextScript />
